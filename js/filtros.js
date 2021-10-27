bbb66af18861be128d0ce190a63bb4eb2aced5a5
AlbertoCodify <albertocodify@gmail.com>
2021-10-27 08:19:44 -0500

    ADD. sección empresas

---
+++ js/filtros.js
@@ -0,0 +1,65 @@
+function mostrarFiltros() {
+    var divFiltros = document.getElementById("div-filtros");
+    divFiltros.classList.remove("ocultar");
+}
+
+function ocultarFiltros() {
+    var divFiltros = document.getElementById("div-filtros");
+    divFiltros.classList.add("ocultar");
+}
+
+function mostrarTodos() {
+   
+    var cat1 = document.getElementById("cat1");
+    cat1.classList.remove("ocultar");
+    var cat2 = document.getElementById("cat2");
+    cat2.classList.remove("ocultar");
+    var cat3 = document.getElementById("cat3");
+    cat3.classList.remove("ocultar");
+    var cat4 = document.getElementById("cat4");
+    cat4.classList.remove("ocultar");
+    var cat5 = document.getElementById("cat5");
+    cat5.classList.remove("ocultar");
+    var cat6 = document.getElementById("cat6");
+    cat6.classList.remove("ocultar");
+    var cat7 = document.getElementById("cat7");
+    cat7.classList.remove("ocultar");
+    var cat8 = document.getElementById("cat8");
+    cat8.classList.remove("ocultar");
+    var cat9 = document.getElementById("cat9");
+    cat9.classList.remove("ocultar");
+    var cat10 = document.getElementById("cat10");
+    cat10.classList.remove("ocultar");
+    var cat11 = document.getElementById("cat11");
+    cat11.classList.remove("ocultar");
+
+    var divFiltros = document.getElementById("div-filtros");
+    divFiltros.classList.add("ocultar");
+}
+
+function mostrarCat1() {
+   
+    var cat2 = document.getElementById("cat2");
+    cat2.classList.add("ocultar");
+    var cat3 = document.getElementById("cat3");
+    cat3.classList.add("ocultar");
+    var cat4 = document.getElementById("cat4");
+    cat4.classList.add("ocultar");
+    var cat5 = document.getElementById("cat5");
+    cat5.classList.add("ocultar");
+    var cat6 = document.getElementById("cat6");
+    cat6.classList.add("ocultar");
+    var cat7 = document.getElementById("cat7");
+    cat7.classList.add("ocultar");
+    var cat8 = document.getElementById("cat8");
+    cat8.classList.add("ocultar");
+    var cat9 = document.getElementById("cat9");
+    cat9.classList.add("ocultar");
+    var cat10 = document.getElementById("cat10");
+    cat10.classList.add("ocultar");
+    var cat11 = document.getElementById("cat11");
+    cat11.classList.add("ocultar");
+
+    var divFiltros = document.getElementById("div-filtros");
+    divFiltros.classList.add("ocultar");
+}
