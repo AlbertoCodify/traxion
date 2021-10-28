9cf0e353e9067359c113268a07de3e7d692f68b5
AlbertoCodify <albertocodify@gmail.com>
2021-10-28 06:54:44 -0500

    UPDATE. Agregando ODS en sostenibilidad y filtros en empresas

--- js/filtros.js
+++ js/filtros.js
@@ -1,4 +1,28 @@
 function mostrarFiltros() {
+
+    var cat1 = document.getElementById("cat1");
+    cat1.classList.remove("ocultar");
+    var cat2 = document.getElementById("cat2");
+    cat2.classList.remove("ocultar");
+    var cat3 = document.getElementById("cat3");
+    cat3.classList.remove("ocultar");
+    var cat4 = document.getElementById("cat4");
+    cat4.classList.remove("ocultar");
+    var cat5 = document.getElementById("cat5");
+    cat5.classList.remove("ocultar");
+    var cat6 = document.getElementById("cat6");
+    cat6.classList.remove("ocultar");
+    var cat7 = document.getElementById("cat7");
+    cat7.classList.remove("ocultar");
+    var cat8 = document.getElementById("cat8");
+    cat8.classList.remove("ocultar");
+    var cat9 = document.getElementById("cat9");
+    cat9.classList.remove("ocultar");
+    var cat10 = document.getElementById("cat10");
+    cat10.classList.remove("ocultar");
+    var cat11 = document.getElementById("cat11");
+    cat11.classList.remove("ocultar");
+    
     var divFiltros = document.getElementById("div-filtros");
     divFiltros.classList.remove("ocultar");
 }
@@ -37,8 +61,22 @@ function mostrarTodos() {
     divFiltros.classList.add("ocultar");
 }
 
-function mostrarCat1() {
-   
+function filtroTransporteDeCarga() {
+    var cat4 = document.getElementById("cat4");
+    cat4.classList.add("ocultar");
+    var cat6 = document.getElementById("cat6");
+    cat6.classList.add("ocultar");
+    var cat9 = document.getElementById("cat9");
+    cat9.classList.add("ocultar");
+    var cat10 = document.getElementById("cat10");
+    cat10.classList.add("ocultar");
+ 
+    var divFiltros = document.getElementById("div-filtros");
+    divFiltros.classList.add("ocultar");
+}
+
+function filtroIntegracionLogistica() {
+    
     var cat2 = document.getElementById("cat2");
     cat2.classList.add("ocultar");
     var cat3 = document.getElementById("cat3");
@@ -55,11 +93,227 @@ function mostrarCat1() {
     cat8.classList.add("ocultar");
     var cat9 = document.getElementById("cat9");
     cat9.classList.add("ocultar");
+    
+ 
+    var divFiltros = document.getElementById("div-filtros");
+    divFiltros.classList.add("ocultar");
+}
+
+function filtroServiciosDeAlacenaje() {
+    
+    var cat1 = document.getElementById("cat1");
+    cat1.classList.add("ocultar");
+    var cat2 = document.getElementById("cat2");
+    cat2.classList.add("ocultar");
+    var cat4 = document.getElementById("cat4");
+    cat4.classList.add("ocultar");
+    var cat5 = document.getElementById("cat5");
+    cat5.classList.add("ocultar");
+    var cat6 = document.getElementById("cat6");
+    cat6.classList.add("ocultar");
+    var cat7 = document.getElementById("cat7");
+    cat7.classList.add("ocultar");
+    var cat8 = document.getElementById("cat8");
+    cat8.classList.add("ocultar");
+    var cat9 = document.getElementById("cat9");
+    cat9.classList.add("ocultar");
     var cat10 = document.getElementById("cat10");
     cat10.classList.add("ocultar");
     var cat11 = document.getElementById("cat11");
     cat11.classList.add("ocultar");
-
+    
+ 
     var divFiltros = document.getElementById("div-filtros");
     divFiltros.classList.add("ocultar");
 }
+
+function filtroSistemasLogisticos() {
+    
+    var cat1 = document.getElementById("cat1");
+    cat1.classList.add("ocultar");
+    var cat2 = document.getElementById("cat2");
+    cat2.classList.add("ocultar");
+    var cat4 = document.getElementById("cat4");
+    cat4.classList.add("ocultar");
+    var cat5 = document.getElementById("cat5");
+    cat5.classList.add("ocultar");
+    var cat6 = document.getElementById("cat6");
+    cat6.classList.add("ocultar");
+    var cat7 = document.getElementById("cat7");
+    cat7.classList.add("ocultar");
+    var cat8 = document.getElementById("cat8");
+    cat8.classList.add("ocultar");
+    var cat9 = document.getElementById("cat9");
+    cat9.classList.add("ocultar");
+    var cat10 = document.getElementById("cat10");
+    cat10.classList.add("ocultar");
+ 
+    var divFiltros = document.getElementById("div-filtros");
+    divFiltros.classList.add("ocultar");
+}
+
+function filtroPaqueteriaMensajeria() {
+    
+    var cat1 = document.getElementById("cat1");
+    cat1.classList.add("ocultar");
+    var cat2 = document.getElementById("cat2");
+    cat2.classList.add("ocultar");
+    var cat3 = document.getElementById("cat3");
+    cat3.classList.add("ocultar");
+    var cat4 = document.getElementById("cat4");
+    cat4.classList.add("ocultar");
+    var cat5 = document.getElementById("cat5");
+    cat5.classList.add("ocultar");
+    var cat6 = document.getElementById("cat6");
+    cat6.classList.add("ocultar");
+    var cat8 = document.getElementById("cat8");
+    cat8.classList.add("ocultar");
+    var cat9 = document.getElementById("cat9");
+    cat9.classList.add("ocultar");
+    var cat10 = document.getElementById("cat10");
+    cat10.classList.add("ocultar");
+    var cat11 = document.getElementById("cat11");
+    cat11.classList.add("ocultar");
+ 
+    var divFiltros = document.getElementById("div-filtros");
+    divFiltros.classList.add("ocultar");
+}
+
+function filtroTransporteEscolar() {
+    
+    var cat1 = document.getElementById("cat1");
+    cat1.classList.add("ocultar");
+    var cat2 = document.getElementById("cat2");
+    cat2.classList.add("ocultar");
+    var cat3 = document.getElementById("cat3");
+    cat3.classList.add("ocultar");
+    var cat5 = document.getElementById("cat5");
+    cat5.classList.add("ocultar");
+    var cat6 = document.getElementById("cat6");
+    cat6.classList.add("ocultar");
+    var cat7 = document.getElementById("cat7");
+    cat7.classList.add("ocultar");
+    var cat8 = document.getElementById("cat8");
+    cat8.classList.add("ocultar");
+    var cat9 = document.getElementById("cat9");
+    cat9.classList.add("ocultar");
+    var cat10 = document.getElementById("cat10");
+    cat10.classList.add("ocultar");
+    var cat11 = document.getElementById("cat11");
+    cat11.classList.add("ocultar");
+ 
+    var divFiltros = document.getElementById("div-filtros");
+    divFiltros.classList.add("ocultar");
+}
+
+function filtroServiciosEspeciales() {
+    
+    var cat1 = document.getElementById("cat1");
+    cat1.classList.add("ocultar");
+    var cat2 = document.getElementById("cat2");
+    cat2.classList.add("ocultar");
+    var cat3 = document.getElementById("cat3");
+    cat3.classList.add("ocultar");
+    var cat5 = document.getElementById("cat5");
+    cat5.classList.add("ocultar");
+    var cat6 = document.getElementById("cat6");
+    cat6.classList.add("ocultar");
+    var cat7 = document.getElementById("cat7");
+    cat7.classList.add("ocultar");
+    var cat8 = document.getElementById("cat8");
+    cat8.classList.add("ocultar");
+    var cat9 = document.getElementById("cat9");
+    cat9.classList.add("ocultar");
+    var cat10 = document.getElementById("cat10");
+    cat10.classList.add("ocultar");
+    var cat11 = document.getElementById("cat11");
+    cat11.classList.add("ocultar");
+ 
+    var divFiltros = document.getElementById("div-filtros");
+    divFiltros.classList.add("ocultar");
+}
+
+
+function filtroTransportePasajeros() {
+    
+    var cat1 = document.getElementById("cat1");
+    cat1.classList.add("ocultar");
+    var cat2 = document.getElementById("cat2");
+    cat2.classList.add("ocultar");
+    var cat3 = document.getElementById("cat3");
+    cat3.classList.add("ocultar");
+    var cat4 = document.getElementById("cat4");
+    cat4.classList.add("ocultar");
+    var cat5 = document.getElementById("cat5");
+    cat5.classList.add("ocultar");
+    var cat6 = document.getElementById("cat6");
+    cat6.classList.add("ocultar");
+    var cat7 = document.getElementById("cat7");
+    cat7.classList.add("ocultar");
+    var cat8 = document.getElementById("cat8");
+    cat8.classList.add("ocultar");
+    var cat10 = document.getElementById("cat10");
+    cat10.classList.add("ocultar");
+    var cat11 = document.getElementById("cat11");
+    cat11.classList.add("ocultar");
+ 
+    var divFiltros = document.getElementById("div-filtros");
+    divFiltros.classList.add("ocultar");
+}
+
+function filtroMudanzas() {
+    
+    var cat1 = document.getElementById("cat1");
+    cat1.classList.add("ocultar");
+    var cat2 = document.getElementById("cat2");
+    cat2.classList.add("ocultar");
+    var cat3 = document.getElementById("cat3");
+    cat3.classList.add("ocultar");
+    var cat4 = document.getElementById("cat4");
+    cat4.classList.add("ocultar");
+    var cat6 = document.getElementById("cat6");
+    cat6.classList.add("ocultar");
+    var cat7 = document.getElementById("cat7");
+    cat7.classList.add("ocultar");
+    var cat8 = document.getElementById("cat8");
+    cat8.classList.add("ocultar");
+    var cat9 = document.getElementById("cat9");
+    cat9.classList.add("ocultar");
+    var cat10 = document.getElementById("cat10");
+    cat10.classList.add("ocultar");
+    var cat11 = document.getElementById("cat11");
+    cat11.classList.add("ocultar");
+ 
+    var divFiltros = document.getElementById("div-filtros");
+    divFiltros.classList.add("ocultar");
+}
+
+function filtroPublicidad() {
+    
+    var cat1 = document.getElementById("cat1");
+    cat1.classList.add("ocultar");
+    var cat2 = document.getElementById("cat2");
+    cat2.classList.add("ocultar");
+    var cat3 = document.getElementById("cat3");
+    cat3.classList.add("ocultar");
+    var cat4 = document.getElementById("cat4");
+    cat4.classList.add("ocultar");
+    var cat5 = document.getElementById("cat5");
+    cat5.classList.add("ocultar");
+    var cat7 = document.getElementById("cat7");
+    cat7.classList.add("ocultar");
+    var cat8 = document.getElementById("cat8");
+    cat8.classList.add("ocultar");
+    var cat9 = document.getElementById("cat9");
+    cat9.classList.add("ocultar");
+    var cat10 = document.getElementById("cat10");
+    cat10.classList.add("ocultar");
+    var cat11 = document.getElementById("cat11");
+    cat11.classList.add("ocultar");
+ 
+    var divFiltros = document.getElementById("div-filtros");
+    divFiltros.classList.add("ocultar");
+}
+
+
+
